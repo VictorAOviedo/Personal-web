846f40a02a10fc9cf13b70f46407877921dd6e80
Victor Oviedo <victor.oviedo.a87@gmail.com>
2023-08-10 20:03:48 -0300

    Scroll

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ let typed = new Typed('.auto-input',{
     backDelay: 2000,
     loop: true,
 })
+
+// Active link state on scroll
+
+// Get all links
+let navLinks = document.querySelectorAll('nav ul li a')
+//Get all sections
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function (){
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+});
